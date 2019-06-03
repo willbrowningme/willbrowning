789781d71c1b2b8fb1cec9f3c645647c01a5c43b
will <will@nitra.co.uk>
2019-06-03 13:33:09 +0100

    Updated newsletter form to sendy

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="mc_embed_signup" class="w-full bg-gray-200 shadow-md rounded p-8 sm:p-10 my-8">
-    <form action="https://willbrowning.us18.list-manage.com/subscribe/post?u=b8f3cbce60d44abbdda11dfb6&amp;id=a42042545d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <div id="mc_embed_signup_scroll" class="flex items-center flex-col sm:flex-row">
-        <div class="w-full md:w-1/2 mb-4 md:mb-0">
-          <h5 class="mt-0 text-xl">Like what you've seen here? </h5>
-          <p>
-            Fire over your email and I'll keep you updated once a month about any new posts. No Spam.
-          </p>
-        </div>
-        <div class="w-full md:w-1/2 md:ml-4">
-          <input type="email" value="" name="EMAIL" class="email block shadow rounded w-full py-2 px-3 text-gray-600 mb-4 font-sans" id="mce-EMAIL" placeholder="Your email" required>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-            <input type="text" name="b_b8f3cbce60d44abbdda11dfb6_a42042545d" tabindex="-1" value="">
-          </div>
-          <div class="clear">
-            <input type="submit" value="Let's Go!" name="subscribe" id="mc-embedded-subscribe" class="button w-full bg-pink text-white font-bold py-2 px-4 rounded cursor-pointer font-sans tracking-wider">
-          </div>
-        </div>
+
+    <div class="flex items-center flex-col sm:flex-row">
+      <div class="w-full md:w-1/2 mb-4 md:mb-0">
+        <h5 class="mt-0 text-xl">Like what you've seen here? </h5>
+        <p>
+          Fire over your email and I'll keep you updated once a month about any new posts. No Spam.
+        </p>
       </div>
-    </form>
+      <div class="w-full md:w-1/2 md:ml-4">
+        <form action="https://sendy.willbrowning.me/subscribe" method="POST" accept-charset="utf-8">
+          <label for="email" class="hidden">Email</label>
+          <input type="email" name="email" id="email" class="email block shadow rounded w-full py-2 px-3 text-gray-600 mb-4 font-sans" placeholder="Your email" required/>
+          <div class="hidden">
+            <label for="hp">HP</label>
+            <input type="text" name="hp" id="hp"/>
+          </div>
+          <input type="hidden" name="list" value="G4t7tNFm3f6qoxz4mHaywQ"/>
+          <input type="hidden" name="subform" value="yes"/>
+          <input type="submit" name="submit" id="submit" value="Let's Go!" class="button w-full bg-pink text-white font-bold py-2 px-4 rounded cursor-pointer font-sans tracking-wider"/>
+        </form>
+      </div>
+    </div>
+
   </div>
 </template>
